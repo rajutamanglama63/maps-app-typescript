44f360b24242c4cd312b12d206064462fd8e0b55
rajutamanglama65 <raju@fusemachines.com>
2023-05-05 11:17:16 +0545

    feat: custom map created

---
+++ src/CustomMap.ts
@@ -0,0 +1,15 @@
+/// <reference types="@types/google.maps" />
+
+export class CustomMap {
+    googleMap: google.maps.Map
+
+    constructor(divId:  string) {
+        this.googleMap = new google.maps.Map(document.getElementById(divId) as HTMLElement, {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0
+            }
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-/// <reference types="@types/google.maps" />
-
-
+import { CustomMap } from "./CustomMap";
 // import { User } from "./User";
 // import { Company } from "./Company";
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-});
+// new google.maps.Map(document.getElementById('map') as HTMLElement, {
+//     zoom: 1,
+//     center: {
+//         lat: 0,
+//         lng: 0
+//     }
+// });
+
+new CustomMap("map")
